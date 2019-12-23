b115efa362974aca3f257561fddca58c64ce6ca3
anDDDy <43758756+anDDDy@users.noreply.github.com>
2019-12-23 14:05:21 +0300

    uploading tasks;

---
+++ task_Fibonacсi.lisp
@@ -0,0 +1,7 @@
+(defun fibonacci(n)
+    (if (< n 3)
+        1
+        (+ (fibonacci (- n 1)) (fibonacci (- n 2))) ))
+(loop for i from 1 to 10
+    do (format t "~D, " (fibonacci i))
+    finally (format t "...~%"))
